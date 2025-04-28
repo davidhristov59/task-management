359c8a23c2f1fb82145ff688a9dd18f0c0f33209
221184 <stefan.tagarski@students.finki.ukim.mk>
2025-04-29 00:02:05 +0200

    add aggregate Workspace and Value Objects

---
+++ backend/src/main/java/mk/ukim/finki/soa/backend/model/Workspace.kt
@@ -0,0 +1,100 @@
+package mk.ukim.finki.soa.taskmanagement.model
+
+import jakarta.persistence.*
+import mk.ukim.finki.soa.backend.model.LabeledEntity
+import mk.ukim.finki.soa.backend.model.WorkspaceId
+import mk.ukim.finki.soa.backend.model.WorkspaceTitle
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.modelling.command.AggregateIdentifier
+import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.spring.stereotype.Aggregate
+
+@Entity
+@Aggregate(repository = "axonWorkspaceRepository")
+class Workspace : LabeledEntity {
+
+    @AggregateIdentifier
+    @EmbeddedId
+    @AttributeOverride(name = "value", column = Column(name = "id"))
+    lateinit var id: WorkspaceId
+
+    @Embedded
+    lateinit var title: WorkspaceTitle
+
+    lateinit var ownerId: String
+
+    var archived: Boolean = false
+
+    @ElementCollection
+    var memberIds: MutableList<String> = mutableListOf()
+
+    @CommandHandler
+    constructor(command: CreateWorkspaceCommand) {
+        val event = WorkspaceCreatedEvent(
+            workspaceId = WorkspaceId(),
+            title = WorkspaceTitle(command.title),
+            ownerId = command.ownerId
+        )
+
+        this.on(event)
+        AggregateLifecycle.apply(event)
+    }
+
+    constructor()
+
+    fun on(event: WorkspaceCreatedEvent) {
+        this.id = event.workspaceId
+        this.title = event.title
+        this.ownerId = event.ownerId
+        this.archived = false
+        this.memberIds = mutableListOf()
+    }
+
+    fun updateTitle(command: UpdateWorkspaceTitleCommand) {
+        val event = WorkspaceTitleUpdatedEvent(
+            workspaceId = command.workspaceId,
+            title = WorkspaceTitle(command.title)
+        )
+
+        this.on(event)
+        AggregateLifecycle.apply(event)
+    }
+
+    fun on(event: WorkspaceTitleUpdatedEvent) {
+        this.title = event.title
+    }
+
+    @CommandHandler
+    fun archive(command: ArchiveWorkspaceCommand) {
+        val event = WorkspaceArchivedEvent(command)
+
+        this.on(event)
+        AggregateLifecycle.apply(event)
+    }
+
+    fun on(event: WorkspaceArchivedEvent) {
+        this.archived = true
+    }
+
+    fun addMember(command: AddMemberToWorkspaceCommand) {
+        val event = MemberAddedToWorkspaceEvent(
+            workspaceId = command.workspaceId,
+            memberId = command.memberId
+        )
+
+        this.on(event)
+        AggregateLifecycle.apply(event)
+    }
+
+    fun on(event: MemberAddedToWorkspaceEvent) {
+        this.memberIds.add(event.memberId)
+    }
+
+    override fun getId(): Identifier<out Any> {
+        return id
+    }
+
+    override fun getLabel(): String {
+        return title.value
+    }
+}
---
+++ backend/src/main/java/mk/ukim/finki/soa/backend/model/WorkspaceValueObjects.kt
@@ -0,0 +1,34 @@
+package mk.ukim.finki.soa.backend.model
+
+import jakarta.persistence.Embeddable
+import mk.ukim.finki.soa.taskmanagement.model.Workspace
+import java.io.Serializable
+import java.util.*
+
+@Embeddable
+open class WorkspaceId(value: String) : Identifier<Workspace>(value, Workspace::class.java) {
+    constructor() : this(UUID.randomUUID().toString())
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        return this.value == (other as WorkspaceId).value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+}
+
+@Embeddable
+data class WorkspaceTitle(
+    val value: String
+) {
+    init {
+        require(value.isNotBlank()) { "Workspace title must not be blank." }
+        require(value.length <= 100) { "Workspace title must not exceed 100 characters." }
+    }
+
+    override fun toString(): String = value
+}
